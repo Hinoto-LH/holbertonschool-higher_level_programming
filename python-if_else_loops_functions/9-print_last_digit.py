1144ec3950c68fb6e0538dca036b4b5eb9700ed5
Hinoto-LH <masseron.a.pro@gmail.com>
2026-01-15 17:13:56 +0100

    fix code

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
 
-    if number < 0:
-        lastDig = number % 10 - 10
-    else:
-        lastDig = number % 10
+    lastDig = number % 10
     return lastDig
