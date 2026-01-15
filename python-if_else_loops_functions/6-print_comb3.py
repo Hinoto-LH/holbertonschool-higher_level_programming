2d13b8d61719500adadb4178a3b0426e8a1235ae
Hinoto-LH <masseron.a.pro@gmail.com>
2026-01-15 18:24:19 +0100

    fix code

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 9):
-    print("{}".format(i), end="")
-    for j in range(1, 90):
-        print("{:02}, ".format(j), end="")
+for i in range(8):
+    for j in range(10):
+        if i < j:
+            print("{}{}, ".format(i, j), end="")
+print("89")
