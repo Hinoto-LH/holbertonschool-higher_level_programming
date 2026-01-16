0ff035dad35648eafa777cf72451fdbe0f3885fa
Hinoto-LH <masseron.a.pro@gmail.com>
2026-01-16 23:57:33 +0100

    Add and execute file

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python
+import sys
+
+if __name__ == "__main__":
+    numbArg = len(sys.argv) - 1
+    result = 0
+    for arg in (sys.argv[1:]):
+        result = int(arg) + result
+    print("{}".format(result))
