3aeac21fb570a4db83ec28cfefbc39fe49d152f6
Hinoto-LH <masseron.a.pro@gmail.com>
2026-01-16 13:50:28 +0100

    Add New file and exec this

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -2,4 +2,4 @@
 for i in range(97, 123):
     if i != 101 and i != 113:
         print("{}".format(chr(i)), end="")
-# chr dec to ASCII and ord ASCII to dec
+# chr ASCII to charactere and ord entier to ASCII
