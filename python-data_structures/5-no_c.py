27ea5a4068262d2b4646641ebd53f6ec5d89d66f
Hinoto-LH <masseron.a.pro@gmail.com>
2026-01-22 19:12:48 +0100

    Add and execute file

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    
+    new_string = ""
+    for car in my_string:
+        if car != "c" and car != "C":
+            new_string += car
+    return new_string
