a10a1dcf1946bc02e1dc0182152733135ccae531
Hinoto-LH <masseron.a.pro@gmail.com>
2026-01-22 18:10:58 +0100

    Fix code: format string

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for x in range(len(matrix)):
         for y in range(len(matrix[x])):
-            print("{}".format(matrix[x][y]), end=" ")
+            print("{:d}".format(matrix[x][y]), end=" ")
         print()
 # matrix = [[]]. x = nombre de tableaux dans matrix
 # y = nombre d'element dans chaque x
