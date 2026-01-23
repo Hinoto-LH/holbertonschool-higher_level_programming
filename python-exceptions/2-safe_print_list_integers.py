d13ca1b4dd93e1b1972a1ad6168f2002c299f007
Hinoto-LH <masseron.a.pro@gmail.com>
2026-01-23 21:39:06 +0100

    Fix code: modify condition

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,7 +3,7 @@ def safe_print_list_integers(my_list=[], x=0):
     counter = 0
     for index in range(x):
         try:
-            if isinstance(my_list[index], int)
+            if isinstance(my_list[index], int):
                 print("{:d}".format(my_list[index]), end="")
                 counter += 1
         except IndexError:
