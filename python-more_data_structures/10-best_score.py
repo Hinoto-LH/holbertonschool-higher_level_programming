d8e33f513adbf2fdcf9d4de4338fd00adaf36cea
Hinoto-LH <masseron.a.pro@gmail.com>
2026-01-23 12:41:06 +0100

    Add and execute file

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    else:
+        save_key = None
+        max_value = None
+        for key, value in a_dictionary.items():
+            if max_value is None:
+                max_value = a_dictionary[key]
+            if max_value < value:
+                save_key = key
+                max_value = value
+    return save_key
