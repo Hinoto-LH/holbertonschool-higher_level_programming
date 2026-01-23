fa55d902532c5866255f0deb777968e7777b54fb
Hinoto-LH <masseron.a.pro@gmail.com>
2026-01-23 12:47:15 +0100

    fix code: Add new variable and delete 1st return

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary is None:
-        return None
+        save_key = None
     else:
         max_value = None
         for key, value in a_dictionary.items():
