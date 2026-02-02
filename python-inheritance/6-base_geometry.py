c12773694644f524d08e77aaa9275d797f820a45
Hinoto-LH <masseron.a.pro@gmail.com>
2026-02-02 14:38:11 +0100

    Add and execute file

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+class BaseGeometry:
+    """Write an empty class BaseGeometry."""
+    def area(self):
+        """
+        Public instance method: def area(self): that raises an Exception
+          with the message area() is not implemented
+        """
+        raise Exception("area() is not implemented")
