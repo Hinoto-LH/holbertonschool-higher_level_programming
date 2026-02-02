00379607a74cd7bc083b7c5b0f35941be0badb54
Hinoto-LH <masseron.a.pro@gmail.com>
2026-02-02 14:32:47 +0100

    Add and execute file

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -6,4 +6,4 @@ def inherits_from(obj, a_class):
       otherwise False.
     """
     return issubclass(type(obj), a_class) and type(obj) is not a_class
-    """type(obj) is not a class exclut les instances direct"""
+    """type(obj) is not a class /exclut les instances direct"""
