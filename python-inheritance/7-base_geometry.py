f6f37076b21976cbe3b873ad171fce4f212e61c6
Hinoto-LH <masseron.a.pro@gmail.com>
2026-02-02 15:02:27 +0100

    Add and execute file

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+class BaseGeometry:
+    """Write an empty class BaseGeometry."""
+    def area(self):
+        """
+        Public instance method: def area(self): that raises an Exception
+          with the message area() is not implemented
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """exception of TypeError and ValueError"""
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        elif value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
