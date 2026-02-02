fde1958263b5ae5dfb0dbb0e86166da3be8cd182
Hinoto-LH <masseron.a.pro@gmail.com>
2026-02-02 15:42:01 +0100

    Add and execute file

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -8,6 +8,7 @@ Write a class Rectangle that inherits from BaseGeometry (7-base_geometry.py)
 class Rectangle(BaseGeometry):
 
     def __init__(self, width, height):
+        """use method of BaseGeometry"""
         self.integer_validator("width", width)
         self.__width__ = width
         self.integer_validator("height", height)
