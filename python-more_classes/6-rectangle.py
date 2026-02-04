f2f60ab8f643091d575796fa5c00d4a00fd9df36
Hinoto-LH <masseron.a.pro@gmail.com>
2026-02-04 09:36:32 +0100

    remove directory test

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -5,7 +5,7 @@
 class Rectangle:
     """Represents a rectangle with width and height."""
 
-    number_of_instances = 0
+    number_of_instances = 0 
 
     def __init__(self, width=0, height=0):
         """
