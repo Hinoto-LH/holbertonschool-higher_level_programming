06a9758fc050e94e8945eb66258b882145297a9c
Hinoto-LH <masseron.a.pro@gmail.com>
2026-02-04 09:45:57 +0100

    Add and modidy my comment

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
-class MyList(list):
-    """Class for my_list"""
+"""
+Ce module définit une classe MyList qui hérite de la classe list
+et permet d’afficher la liste triée sans la modifier.
+"""
 
+
+class MyList(list):
+    """
+    Classe qui hérite de list et ajoute une méthode
+    pour afficher la liste triée.
+    """
     def print_sorted(self):
-        """print a list sorted"""
+        """
+        Affiche la liste triée par ordre croissant
+        sans modifier la liste originale.
+        """
         print(sorted(self))
