fe79f8337ca4b8466dd09b3905515fe400d5ddd0
Hinoto-LH <masseron.a.pro@gmail.com>
2026-02-04 09:56:32 +0100

    Add new comment, Fix code: change return

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
+"""
+Ce module contient une fonction qui vérifie si un objet est
+une instance d'une classe donnée ou d'une classe qui en hérite.
+"""
+
+
 def is_kind_of_class(obj, a_class):
     """
-    Write a function that returns True if the object is an instance of,
-    or if the object is an instance of a class that inherited from,
-      the specified class ; otherwise False.
+    Retourne True si l'objet est une instance de la classe spécifiée
+    ou d'une classe qui en hérite, sinon False.
+
+    Args:
+        obj: l'objet à vérifier
+        a_class: la classe de référence
+
+    Returns:
+        bool: True ou False selon le cas
     """
-    if isinstance(obj, a_class):
-        return True
+    return isinstance(obj, a_class)
