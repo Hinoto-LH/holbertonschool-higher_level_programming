abf51b94d66e01b91c3a5524f442a5d3edd65129
Hinoto-LH <masseron.a.pro@gmail.com>
2026-02-04 10:32:43 +0100

    Add new comment

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,11 +1,29 @@
 #!/usr/bin/python3
-Rectangle = __import__('9-rectangle').Rectangle
-"""class Square """
+"""
+Module qui définit la classe Square,
+héritant de Rectangle.
+"""
+
+Rectangle = __import__('8-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    """
+    Classe Square qui hérite de Rectangle.
+
+    Attribut privé:
+        __size (int): taille du carré
+    """
+
     def __init__(self, size):
-        """use method of Rectangle"""
+        """
+        Initialise un carré avec une taille.
+
+        Args:
+            size (int): taille du carré (>0)
+
+        Utilise integer_validator de BaseGeometry pour valider la valeur.
+        """
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size__ = size
