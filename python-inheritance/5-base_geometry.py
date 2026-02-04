8e1f3e394ba24653cf5769e5003794f46c130cae
Hinoto-LH <masseron.a.pro@gmail.com>
2026-02-04 10:01:32 +0100

    Add new comment

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+"""
+Module qui définit une classe BaseGeometry vide,
+servant de classe de base pour la géométrie.
+"""
+
+
 class BaseGeometry:
-    """Write an empty class BaseGeometry."""
+    """
+    Classe de base pour la géométrie.
+    Ne contient aucune méthode ou attribut.
+    """
     pass
