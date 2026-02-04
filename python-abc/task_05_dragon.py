d20a098ba8ad161aff90a37e0ce9277970d719e8
Hinoto-LH <masseron.a.pro@gmail.com>
2026-02-04 11:23:00 +0100

    Add comment

--- python-abc/task_05_dragon.py
+++ python-abc/task_05_dragon.py
@@ -1,18 +1,31 @@
 #!/usr/bin/env python3
+"""
+Module illustrant l'utilisation des mixins en Python
+avec une classe Dragon qui peut nager et voler.
+"""
 
 
 class SwimMixin:
+    """Mixin qui ajoute la capacité de nager."""
+
     def swim(self):
+        """Affiche que la créature nage."""
         print("The creature swims!")
 
 
 class FlyMixin:
+    """Mixin qui ajoute la capacité de voler."""
+
     def fly(self):
+        """Affiche que la créature vole."""
         print("The creature flies!")
 
 
 class Dragon(SwimMixin, FlyMixin):
+    """Classe Dragon qui peut nager et voler."""
+
     def roar(self):
+        """Affiche le rugissement du dragon."""
         print("The dragon roars!")
 
 
