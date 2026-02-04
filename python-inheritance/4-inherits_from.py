475b6c594db21673f9892bfe7c040c3a7e526faa
Hinoto-LH <masseron.a.pro@gmail.com>
2026-02-04 10:00:05 +0100

    Add new comment

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python3
+"""
+Ce module contient une fonction qui vérifie si un objet est une instance
+d'une classe héritée d'une classe donnée.
+"""
+
+
 def inherits_from(obj, a_class):
     """
-    Write a function that returns True if the object is an instance of a class
-    that inherited (directly or indirectly) from the specified class ;
-      otherwise False.
+    Retourne True si l'objet est une instance d'une classe qui hérite
+    directement ou indirectement de la classe spécifiée, sinon False.
+
+    Args:
+        obj: l'objet à vérifier
+        a_class: la classe de référence
+
+    Returns:
+        bool: True ou False selon le cas
     """
     return issubclass(type(obj), a_class) and type(obj) is not a_class
-    """type(obj) is not a class /exclut les instances direct"""
+    # type(obj) is not a class /exclut les instances direct"""
