b8f8d07f0847d8515313c99b938e8e88a40ac159
Hinoto-LH <masseron.a.pro@gmail.com>
2026-02-04 10:37:58 +0100

    Add new comment and Fix code: Add new special method str

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,11 +1,38 @@
 #!/usr/bin/python3
+"""
+Module qui définit la classe Square,
+héritant de Rectangle.
+"""
+
 Rectangle = __import__('9-rectangle').Rectangle
-"""class Square """
 
 
 class Square(Rectangle):
+    """
+    Classe Square qui hérite de Rectangle.
+
+    Attribut privé:
+        __size (int): taille du carré
+    """
+
     def __init__(self, size):
-        """use method of Rectangle"""
+        """
+        Initialise un carré avec une taille.
+
+        Args:
+            size (int): taille du carré (>0)
+
+        Utilise integer_validator pour valider la valeur.
+        """
         self.integer_validator("size", size)
         super().__init__(size, size)
-        self.__size__ = size
+        self.__size = size
+
+    def __str__(self):
+        """
+        Retourne la représentation en chaîne du carré.
+
+        Returns:
+            str: format "[Square] <width>/<height>"
+        """
+        return f"[Square] {self.__size}/{self.__size}"
