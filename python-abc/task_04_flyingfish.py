a96bc3141f8d48c4274dd02ad274c5d3ec6a6f76
Hinoto-LH <masseron.a.pro@gmail.com>
2026-02-04 11:21:11 +0100

    Add comment

--- python-abc/task_04_flyingfish.py
+++ python-abc/task_04_flyingfish.py
@@ -1,32 +1,54 @@
 #!/usr/bin/python3
+"""
+Module qui définit des classes pour illustrer le multiple héritage:
+Fish, Bird et FlyingFish.
+"""
+
 
 class Fish:
+    """
+    Classe représentant un poisson.
+    """
 
     def swim(self):
+        """Affiche que le poisson nage."""
         print("The fish is swimming")
 
     def habitat(self):
+        """Affiche l'habitat du poisson."""
         print("The fish lives in water")
 
 
 class Bird:
+    """
+    Classe représentant un oiseau.
+    """
 
     def fly(self):
+        """Affiche que l'oiseau vole."""
         print("The bird is flying")
 
     def habitat(self):
+        """Affiche l'habitat de l'oiseau."""
         print("The bird lives in the sky")
 
 
 class FlyingFish(Fish, Bird):
+    """
+    Classe représentant un poisson volant,
+    héritant de Fish et Bird.
+    """
 
     def fly(self):
+        """Affiche que le poisson volant vole."""
         print("The flying fish is soaring!")
 
     def swim(self):
+        """Affiche que le poisson volant nage."""
         print("The flying fish is swimming!")
 
     def habitat(self):
+        """Affiche que le poisson volant vit dans l'eau et le ciel."""
         print("The flying fish lives both in water and the sky!")
 
 
