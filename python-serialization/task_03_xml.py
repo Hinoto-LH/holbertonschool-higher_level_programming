df607ae1254cd418acceedb021cb7521d9eabfb4
Hinoto-LH <masseron.a.pro@gmail.com>
2026-02-20 16:20:31 +0100

    Modify indentation

---
+++ python-serialization/task_03_xml.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Docstring pour python-serialization.task_03_xml
+"""
+import xml.etree.ElementTree as ET
+
+
+def serialize_to_xml(dictionary, filename):
+    pass
+
+
+def deserialize_from_xml(filename):
+    pass
