dbb3553082f42edddba61745ecf8db28ff3f738b
Hinoto-LH <masseron.a.pro@gmail.com>
2026-03-19 14:51:26 +0100

    fix code: Remove all const

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,13 @@
 #!/usr/bin/node
 
-console.log('C is fun');
-console.log('Python is cool');
-console.log('JavaScript is amazing');
+/* Write a script that prints 3 lines:
+    The first line: "C is fun"
+    The second line: "Python is cool"
+    The third line: "JavaScript is amazing"
+    You must use console.log(...) to print all output
+    You are not allowed to use var
+*/
+const first = 'C is fun';
+const sec = 'Python is cool';
+const third = 'JavaScript is amazing';
+console.log(first + '\n' + sec + '\n' + third);
