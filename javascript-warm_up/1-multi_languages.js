3e7e1503d75c7275aa110655f1120d9f4c802154
Hinoto-LH <masseron.a.pro@gmail.com>
2026-03-19 14:43:55 +0100

    fix code: modify for print

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -2,4 +2,4 @@
 const first = 'C is fun';
 const sec = 'Python is cool';
 const third = 'JavaScript is amazing';
-console.log(first, '\n', sec, '\n', third);
+console.log(first + '\n' + sec + '\n' + third);
