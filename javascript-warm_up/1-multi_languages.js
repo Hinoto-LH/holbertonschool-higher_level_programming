23accb6c0872001b0c6f9f4c60281484dc990bf7
Hinoto-LH <masseron.a.pro@gmail.com>
2026-03-19 14:48:47 +0100

    fix code: modify for print

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,12 @@
 #!/usr/bin/node
+
+/* Write a script that prints 3 lines:
+    The first line: "C is fun"
+    The second line: "Python is cool"
+    The third line: "JavaScript is amazing"
+    You must use console.log(...) to print all output
+    You are not allowed to use var
+*/
 const first = 'C is fun';
 const sec = 'Python is cool';
 const third = 'JavaScript is amazing';
